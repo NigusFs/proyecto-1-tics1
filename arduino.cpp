7470eaaa9c5cc1b7ccd7bf107e09b43249350750
Nicolas Flores <nicolas.floress@mail.udp.cl>
2017-06-06 18:09:51 -0300

    MEJORAR CODIGO

--- arduino.cpp
+++ arduino.cpp
@@ -1,15 +1,17 @@
-const int pot=0;
+const int pot_f=0;// pin sensor flujo
+const int pot_t=1;// pin sensor temeperatura
+const int pot_p=2;//  pin sensor presion
 int count=0;
 //unsigned long pulso_total;
 
 void setup(){ Serial.begin(9600); }
 
-unsigned long timedPulsos( unsigned long pulso_total,int count) // quiero q esto se repita
-{ // esto va dentro del void loop
-  unsigned long startedAt = millis();
-  while(millis() - startedAt < 15000) // 15 segundos
+unsigned long timedPulsos( unsigned long pulso_total,int count, int seconds)
+{ 
+  unsigned long startedAt = millis(); // optimizar esto ?
+  while(millis() - startedAt < seconds *1000) // milisegundos
   { 
-  int pulso=analogRead(pot);
+  int pulso=analogRead(pot_p);
   //Serial.println(pulso);
   if (pulso > 30){ pulso_total=pulso+pulso_total;}
   }
@@ -18,7 +20,28 @@ unsigned long timedPulsos( unsigned long pulso_total,int count) // quiero q esto
  //Serial.println (") :");
   return pulso_total;
 }
+
+unsigned long timedTemperatura( float temp_total,int count, int seconds)// temperatura promedio en n seconds
+{ 
+  unsigned long startedAt = millis();
+  while(millis() - startedAt < seconds *1000) // milisegundos
+  { 
+  int temp=analogRead(pot_t);
+  //Serial.println(pulso);
+  temp_total=temp+temp_total;
+  
+ //Serial.print ("Pulsos totales (");
+ //Serial.print (count);
+ //Serial.println (") :");
+  return pulso_total;
+}
+
+
+
+
 void loop(){
-  Serial.println(timedPulsos(0,count)); 
+  int secs=15;
+  Serial.println(timedPulsos(0,count,secs));   // 15 segundos
+  
   count=count+1;
   }
