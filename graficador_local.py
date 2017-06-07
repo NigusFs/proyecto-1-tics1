6740e2be3c50eb29432617faa58f40e8456a80d5
Nicolas Flores <nicolas.floress@mail.udp.cl>
2017-06-07 01:19:15 -0300

    Update graficador_local.py

--- graficador_local.py
+++ graficador_local.py
@@ -2,6 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 a = np.load('data61870.npy')## hay que colocarle el numero de la prueba
-plt.plot(a)
-plt.ylabel('some numbers')
+plt.title('Pulsos Guardados')
+plt.ylabel('Pulsos recibidos')
+plt.xlabel('Muestras')
+scale=1000
+plt.plot(a/scale)
 plt.show()
