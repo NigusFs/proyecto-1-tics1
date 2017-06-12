f3ad9f5246aa9934381e2da1961dafae39c4bf7d
Nicolas Flores <nicolas.floress@mail.udp.cl>
2017-06-12 00:33:48 -0300

    Falta mandar la alerta

--- new/graficador.py
+++ new/graficador.py
@@ -16,8 +16,8 @@ Humedad_values=[]
 
 values_save_F=np.array([0,0]) ## se hace float por la temperatura
 values_save_T=np.array([float(0),float(0)]) ## se hace float por la temperatura
-#values_save_P=np.array([0,0]) # es necesario colocarle info antes ?
-#values_save_H=np.array([0,0])
+values_save_P=np.array([0,0]) # es necesario colocarle info antes ?
+values_save_H=np.array([0,0])
 
 ##Para guardar los datos
 
@@ -33,12 +33,18 @@ def doAtExit():
     #print(values_save)
     np.save('dataT{}'.format(af),values_save_T)
     np.save('dataF{}'.format(af),values_save_F)
-    print("save succesfully")
+    np.save('dataT{}'.format(af),values_save_H)
+    np.save('dataF{}'.format(af),values_save_P)
+    print("save succesfully { ",af," }")
+    print("Temperatura Guardada: ",values_save_T)
+    print("Humedad Guardada: ",values_save_H)
+    print("Presion Guardada: ",values_save_P)
+    print("Flujo Guardada: ",values_save_F)
     print("serialArduino.isOpen() = " + str(serialArduino.isOpen()))
 
 def plotValues():
     
-    gs= gridspec.GridSpec(2,2)  # cantidad de columnas y filas
+    gs= gridspec.GridSpec(4,4)  # cantidad de columnas y filas
                                 #4 sensores
     ax0=plt.subplot(gs[0,:])#primero #flujo
     ax0.plot(Flujo_values, 'bo-', label='Flujo de agua')
@@ -47,17 +53,15 @@ def plotValues():
     ax1=plt.subplot(gs[1,:])#segundo # temperatura
     ax1.plot(Temp_values, 'ro-', label='Temperatura')
     ax1.legend(loc='upper right')
-    '''
+    
     ax2=plt.subplot(gs[2,:])#tercero # presion atmosferica
-    ax2.plot(Flujo_values, 'go-', label='Presion atmosferica')
+    ax2.plot(Presion_values, 'go-', label='Presion atmosferica')
     ax2.legend(loc='upper right')
 
     ax2=plt.subplot(gs[3,:])#cuarto # humedad
-    ax2.plot(Flujo_values, 'co-', label='Humedad')
+    ax2.plot(Humedad_values, 'co-', label='Humedad')
     ax2.legend(loc='upper right')
 
-
-    '''
     plt.tight_layout()
     
 
@@ -85,6 +89,7 @@ def Flujo(allsensor,values_save_F,Flujo_values): #primer dato entregado
     scale=1/1000
     valuefloatF= float(allsensor[0])*(scale)
     Flujo_values.append(valuefloatF)
+    print("Flujo Capatado: ",valuefloatF)        
     values_save_F=np.insert(values_save_F,values_save_F.size,float(valuefloatF))       
     Flujo_values.pop(0)
     #values.pop(0) verificar si esto sirve
@@ -94,40 +99,45 @@ def Flujo(allsensor,values_save_F,Flujo_values): #primer dato entregado
 def Temperatura(allsensor,values_save_T,Temp_values): #segundo dato en ser entregado
     #values_save_T=np.array([float(0),float(0)])
     scale=1
-    valuefloatT= float(allsensor[1])*(scale)
+    valuefloatT= round(float(allsensor[1])*(scale))
     Temp_values.append(valuefloatT)
+    print("Temperatura Capatada: ",valuefloatT)
     values_save_T=np.insert(values_save_T,values_save_T.size,float(valuefloatT))       
     Temp_values.pop(0)
     #values.pop(0) verificar si esto sirve
     #drawnow(plotValuesT)# funcion solo para graficar los datos de temperatura h
     #plt.pause(1)
     return values_save_T
-'''
-def Presion(allsensor,values_save_P,Presion_values): #tercer dato entregado
+def Humedad(allsensor,values_save_H,Humedad_values): #tercero dato entregado
     scale=1
-    valuefloatP= float(allsensor[2])*(scale)
+    valuefloatH= round(float(allsensor[2])*(scale))
+    Humedad_values.append(valuefloatH)
+    print("Humedad Capatada: ",valuefloatH)        
+    values_save_H=np.insert(values_save_H,values_save_H.size,float(valuefloatH))       
+    Humedad_values.pop(0)
+    return values_save_H
+    #values.pop(0) verificar si esto sirve
+    #drawnow(plotValuesH)# funcion solo para graficar los datos de temperatura h    
+
+def Presion(allsensor,values_save_P,Presion_values): #cuarto dato entregado
+    scale=1
+    valuefloatP= round(float(allsensor[3])*(scale))
     Presion_values.append(valuefloatP)
-    values_save_P=np.insert(values_save_P,values_save.size,float(valuefloatP))       
+    print("Presion Capatada: ",valuefloatP)        
+    values_save_P=np.insert(values_save_P,values_save_P.size,float(valuefloatP))       
     Presion_values.pop(0)
     #values.pop(0) verificar si esto sirve
     #drawnow(plotValuesP)# funcion solo para graficar los datos de temperatura h
-'''
-'''    
-def Humedad(allsensor,values_save_H,Humedad_values): #cuarto dato entregado
-    scale=1
-    valuefloatH= float(allsensor[3])*(scale)
-    Humedad_values.append(valuefloatH)
-    values_save_H=np.insert(values_save_H,values_save.size,float(valuefloatH))       
-    Humedad_values.pop(0)
-    #values.pop(0) verificar si esto sirve
-    #drawnow(plotValuesH)# funcion solo para graficar los datos de temperatura h    
-'''
+    return values_save_P
+
 
 atexit.register(doAtExit)# no sense
 print("serialArduino.isOpen() = " + str(serialArduino.isOpen()))
 for i in range(0,11):#tamaño del grafico a mostrar
     Flujo_values.append(0)
     Temp_values.append(0)
+    Presion_values.append(0)
+    Humedad_values.append(0)    
     ######        
 while True:
     while (serialArduino.inWaiting()==0):
@@ -137,13 +147,12 @@ while True:
         #print(valueInInt)
         allsensor=captdata()
         #print("paso")
-        values_save_T=Temperatura(allsensor,values_save_T,Temp_values)
-        #print(values_save_T)
         values_save_F=Flujo(allsensor,values_save_F,Flujo_values)
-        #Humedad(allsensor,values_save_H,Humedad_values)
-        #Presion(allsensor,values_save_P,Presion_values)
+        values_save_T=Temperatura(allsensor,values_save_T,Temp_values)
+        values_save_P=Presion(allsensor,values_save_P,Presion_values)       
+        values_save_H=Humedad(allsensor,values_save_H,Humedad_values)
         drawnow(plotValues)
-
+        
         
     except ValueError:
         print("Valor Invalido")
